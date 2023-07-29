35e0387a2254d0ac3b84de06a2222bcc064e7e12
Marcos Fonseca <marco12.fonseca@hotmail.com>
2023-07-28 23:49:00 -0300

    adicionado titulo a pagina e adaptado css

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -3,6 +3,8 @@ import './login.css'
 function LoginPage() {
   return (
     <div className ="login-page">
+      <h1 className="text">UBREP</h1>
+      <p className="text">Uniao de bibliotecas da rede estadual pública</p>
       <div className ="form">
         <form className ="login-form">
           <input type="text" placeholder="usuario"/>
