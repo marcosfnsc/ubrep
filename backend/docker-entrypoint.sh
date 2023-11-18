bb1c669160a665eb5de26c12d6ae78da36992023
Marcos Fonseca <marco12.fonseca@hotmail.com>
2023-11-18 00:45:46 -0300

    configuração inicial da conexão do diesel com o postgres

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -7,6 +7,10 @@ CONTAINER_FIRST_STARTUP="$HOME/.CONTAINER_FIRST_STARTUP"
 if [ ! -f $CONTAINER_FIRST_STARTUP ]; then
   touch $CONTAINER_FIRST_STARTUP
 
+  cat .env.example > .env
+  source .env
+
+  cargo install diesel_cli --no-default-features --features postgres
   cargo build
 fi
 
